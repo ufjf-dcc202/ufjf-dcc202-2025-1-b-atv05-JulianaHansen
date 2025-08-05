804bc3509d63ae523b7c6aaef2111522407e7b4b
JulianaHansen <juliana.hansen@engenharia.ufjf.br>
2025-08-05 20:56:46 -0300

    diferencia sapos de cada lado

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const board = document.getElementById('board');
 
+let state = ['>', '>', '>', '_', '<', '<', '<'];
+
 for (let i = 0; i < 7; i++) {
     const cell = document.createElement('div');
     cell.className = 'cell';
     const frog = document.createElement('div');
     frog.className = 'frog';
+    if (state[i] === '>') {
+        frog.classList.add('frog-right');
+    } else if (state[i] === '<') {
+        frog.classList.add('frog-left');
+    }
     cell.appendChild(frog);
     board.appendChild(cell);
 }
+
